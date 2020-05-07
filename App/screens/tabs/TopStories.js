92aaa4bea5886e265dc6f39aaf68acca4e137db8
lmayegun <lmayegun@yahoo.com>
2020-05-07 15:15:27 +0100

    pull in sample data to top stories components

--- App/screens/tabs/TopStories.js
+++ App/screens/tabs/TopStories.js
@@ -1,23 +1,42 @@
-import * as React from 'react';
-import { View, StyleSheet, Dimensions, Button, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, StyleSheet, Dimensions, Button, Text, FlatList} from 'react-native';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { useDispatch, useSelector } from 'react-redux';
 import {withNavigation} from 'react-navigation';
-import axios from 'axios';
 
 import {ContentThumbnail} from '../../components';
+import * as Actions from '../../store/actions/contents/news.actions';
 
-const params = {orderId:2};
-const request = axios.get('/api/e-commerce-app/products');
-request.then((response) => {
-    console.log(response.data)
+const TopStories = ({navigation}) => {
+  const dispatch = useDispatch();
+  const select = useSelector( ({news}) => news.newsState );
+
+  useEffect(()=>{
+    dispatch(Actions.getNewsContent({productId: "Get me mac miller"}));
+  },[dispatch])
+
+  useEffect(()=>{
+
+  },[select])
+
+  if( !select ){
+    return null
   }
-)
-
-const TopStories = ({navigation}) => (
-  <View style={[styles.scene]}>
-    <ContentThumbnail />
-  </View>
-);
+  console.log(select, "mystate")
+  return(
+    <View style={[styles.scene]}>
+      <FlatList
+        data={select}
+        keyExtractor={(item)=> item.id}
+        renderItem={({item})=>{
+          return(
+            <ContentThumbnail title={item.name} />
+          )}
+        }
+      />
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   scene: {
